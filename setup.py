2319771dac52599a847a1a1330a148ecb6236a88
KeithSloan <keith@sloan-home.co.uk>
2019-11-23 09:31:23 +0000

    tidy

--- setup.py
+++ setup.py
@@ -3,27 +3,25 @@ from setuptools import setup
 __version__ = '0.1'
 
 import os
-if 'posix' in os.name:
-    setup(name='FreeCad_GDML_Workbench',
+
+def mysetup(requires) :
+   setup(name='FreeCad_GDML_Workbench',
       version=str(__version__),
       packages=['freecad','lxml']
       maintainer="keithsloan52",
       maintainer_email="keith@sloan-home.co.uk",
       url="https://github.com/KeithSloan/FreeCAD_GDML_Workbench",
       description="GDML Workbench for FreeCAD",
-      install_requires=['lxml'],
+      install_requires=[requires],
       include_package_data=True
       include_package_data=True)
 
+
+
+import os
+if 'posix' in os.name:
+    mysetup('lxml')
+
 else:
-    setup(name='FreeCad_GDML_Workbench',
-      version=str(__version__),
-      packages=['freecad','lxml']
-      maintainer="keithsloan52",
-      maintainer_email="keith@sloan-home.co.uk",
-      url="https://github.com/KeithSloan/FreeCAD_GDML_Workbench",
-      description="GDML Workbench for FreeCAD",
-      install_requires=['python3-lxml'],
-      include_package_data=True
-      include_package_data=True)
+    mysetup('python3-lxml')
 
