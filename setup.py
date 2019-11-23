40a01705e29401fd7f0c8e12f8fd82f00d4c98c2
KeithSloan <keith@sloan-home.co.uk>
2019-11-23 07:03:39 +0000

    Handle lxml name

--- setup.py
+++ setup.py
@@ -2,16 +2,28 @@ from setuptools import setup
 #from pygears import __version__
 __version__ = '0.1'
 
-setup(name='FreeCad_GDML_Workbench',
+import os
+if 'posix' in os.name:
+    setup(name='FreeCad_GDML_Workbench',
       version=str(__version__),
       packages=['freecad','lxml']
-      #,
-      #          'freecad.gears',
-      #  		'pygears'],
+      maintainer="keithsloan52",
+      maintainer_email="keith@sloan-home.co.uk",
+      url="https://github.com/KeithSloan/FreeCAD_GDML_Workbench",
+      description="GDML Workbench for FreeCAD",
+      install_requires=['lxml'],
+      include_package_data=True
+      include_package_data=True)
+
+else:
+    setup(name='FreeCad_GDML_Workbench',
+      version=str(__version__),
+      packages=['freecad','lxml']
       maintainer="keithsloan52",
       maintainer_email="keith@sloan-home.co.uk",
       url="https://github.com/KeithSloan/FreeCAD_GDML_Workbench",
       description="GDML Workbench for FreeCAD",
       install_requires=['python3-lxml'],
       include_package_data=True
-include_package_data=True)
+      include_package_data=True)
+
