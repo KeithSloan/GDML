1b705d420d409532ed5518e7a3917346af44449c
KeithSloan <keith@sloan-home.co.uk>
2020-04-27 12:33:55 +0100

    listSolids

--- Utils/listSolids.py
+++ Utils/listSolids.py
@@ -1,13 +1,22 @@
 import sys
 from lxml import etree 
 
-if len(sys.argv)<3:
-  print ("Usage: sys.argv[0] <in_file> <out_file>")
+if len(sys.argv)<2:
+  print ("Usage: sys.argv[0] <in_file>")
   sys.exit(1)
 
 iname = sys.argv[1]
-oname = sys.argv[2]
 
-print('Parsing : '+iname)
+print('\nParsing : '+iname+'\n')
 tree = etree.parse(iname)
 root = tree.getroot()
+solidList = []
+for s in tree.xpath('//solids/*') :
+    #print(s.tag)
+    if s.tag not in solidList :
+        solidList.append(s.tag)
+l = 'List of Solids in : '+iname
+print(l)
+print('=' * len(l))
+for i in solidList :
+    print(i)        
