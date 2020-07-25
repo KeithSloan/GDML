1183358dfe9e48938792e6d24a41a89231798479
Keith Sloan <keith@sloan-home.co.uk>
2020-07-25 09:46:15 +0100

    gemc - stage1

--- freecad/gdml/GDMLColourMap.py
+++ freecad/gdml/GDMLColourMap.py
@@ -1,8 +1,7 @@
-# -*- coding: utf8 -*-
 #**************************************************************************
 #*                                                                        *
 #*   Copyright (c) 2017 Keith Sloan <keith@sloan-home.co.uk>              *
-#*             (c) Dam Lambert 2020                                       *
+#*             (c) Dam Lambert 2020                                          *
 #*                                                                        *
 #*   This program is free software; you can redistribute it and/or modify *
 #*   it under the terms of the GNU Lesser General Public License (LGPL)   *
@@ -22,69 +21,135 @@
 #*                                                                        *
 #*   Acknowledgements :                                                   *
 #*                                                                        *
-#*                                                                        *
 #**************************************************************************
-__title__="FreeCAD - GDML workbench ColourMap"
-__author__ = "Keith Sloan <keith@sloan-home.co.uk>"
-__url__ = ["https://github.com/KeithSloan/FreeCAD_GDML"]
+
+__title__="FreeCAD GDML Workbench - GDMLColourMap"
+__author__ = "Keith Sloan"
+__url__ = ["http://www.freecadweb.org"]
 
 import FreeCAD
+import FreeCADGui
+
 #from PySide2 import QtGui, QtCore
-#from PySide2.QtCore import QSize, Qt
-#from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton
-
-import sys
-
 from PySide import QtGui, QtCore
 
-class GDMLColourMap(QtGui.QDialog) :
-
-   #   from .GDMLObjects import GDMLColourMapEntry
-
-   def __init__(self) :
-      #super(GDMLColourMap, self).__init__()
+class GDMLColour(QtGui.QWidget):
+  
+   def __init__(self,colour):
       super().__init__()
+      self.setAutoFillBackground(True)
+      self.resize(50, 20)
+
+      palette = self.palette()
+      palette.setColor(QtGui.QPalette.Window, QtGui.QColor(colour))
+      self.setPalette(palette)
+
+class GDMLMaterial(QtGui.QComboBox):
+    
+   def __init__(self,matList) :
+      super().__init__()
+      self.addItems(matList)
+      self.setEditable(False)
+
+   def getItem(self):
+      return str(self.currentText)
+ 
+
+class GDMLColourMapEntry(QtGui.QWidget) :
+
+   def __init__(self,colour,material) :
+      super().__init__()
+      print('Map Entry : '+str(colour))
+      self.colour = colour
+      layout = QtGui.QHBoxLayout()
+      layout.addWidget(GDMLColour(colour))
+      layout.addWidget(material)
+      self.setLayout(layout)
+
+   def dataPicker(self):
+      print('DataPicker')
+
+class GDMLColourMapList(QtGui.QScrollArea) :
+
+   def __init__(self,matList) :
+      super().__init__()
+      # Scroll Area which contains the widgets, set as the centralWidget
+      # Widget that contains the collection of Vertical Box
+      self.widget = QtGui.QWidget()
+      self.matList = matList
+      # The Vertical Box that contains the Horizontal Boxes of  labels and buttons
+      self.vbox = QtGui.QVBoxLayout()
+      self.widget.setLayout(self.vbox)
+
+      #Scroll Area Properties
+      self.setVerticalScrollBarPolicy(QtGui.Qt.ScrollBarAlwaysOn)
+      self.setHorizontalScrollBarPolicy(QtGui.Qt.ScrollBarAlwaysOff)
+      self.setWidgetResizable(True)
+      self.setWidget(self.widget)
+
+   def addEntry(self, colour) :
+      print('Add Entry')
+      mat = GDMLMaterial(self.matList)
+      self.vbox.addWidget(GDMLColourMapEntry(colour,mat))
+
+class GDMLColourMap(QtGui.QDialog) :
+#class GDMLColourMap(QtGui.QMainWindow) :
+   def __init__(self) :
+      super(GDMLColourMap, self).__init__(FreeCADGui.getMainWindow(), QtCore.Qt.Tool)
       self.initUI()
 
-   def initUI(self):
-      #self.result = userCancelled
+   def initUI(self):   
+      self.result = userCancelled
       # create our window
-      # define window		xLoc,yLoc,xDim,yDim
-      self.setGeometry(	250, 250, 400, 150)
-      self.setWindowTitle("Our Example Nonmodal Program Window")
-      self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+      # define window           xLoc,yLoc,xDim,yDim
+      self.setGeometry( 250, 250, 550, 350)
+      self.setWindowTitle("Map FreeCAD Colours to GDML Materials")
       self.setMouseTracking(True)
+      lay = QtGui.QGridLayout(self)
+      
+      materialList = self.getGDMLMaterials()
+      mapList = GDMLColourMapList(materialList)
+      doc = FreeCAD.ActiveDocument
+      print('Active')
+      print(doc)
+      if doc != None :
+         #print(dir(doc))
+         if hasattr(doc,'Objects') :
+            #print(doc.Objects)
+            colorList = []
+            for obj in doc.Objects :
+                #print(dir(obj))
+                if hasattr(obj,'ViewObject') :
+                   print(dir(obj.ViewObject))
+                   if hasattr(obj.ViewObject,'isVisible') :
+                      if obj.ViewObject.isVisible :
+                         if hasattr(obj.ViewObject,'ShapeColor') :
+                            colour = obj.ViewObject.ShapeColor
+                            print(colour)
+                            try:
+                               col = colorList.index(colour)
+                            except ValueError:
+                               colorList.append(colour)
+      print(colorList)
+      for c in colorList :
+          mapList.addEntry(QtGui.QColor(c[0]*255,c[1]*255,c[2]*255))
       # create Labels
-      self.label4 = QtGui.QLabel("can you see this?", self)
-      self.label4.move(20, 20)
-      self.label5 = QtGui.QLabel("Mouse position:", self)
-      self.label5.move(20, 70)
-      self.label6 = QtGui.QLabel("               ", self)
-      self.label6.move(135, 70)
-      # toggle visibility button
-      pushButton1 = QtGui.QPushButton('Toggle visibility', self)
-      pushButton1.clicked.connect(self.onPushButton1)
-      pushButton1.setMinimumWidth(150)
-      #pushButton1.setAutoDefault(False)
-      pushButton1.move(210, 20)
+      self.label1 = mapList 
+      lay.addWidget(self.label1,0,0)
       #  cancel button
       cancelButton = QtGui.QPushButton('Cancel', self)
       cancelButton.clicked.connect(self.onCancel)
       cancelButton.setAutoDefault(True)
-      cancelButton.move(150, 110)
+      lay.addWidget(cancelButton, 2, 1)
+
       # OK button
       okButton = QtGui.QPushButton('OK', self)
       okButton.clicked.connect(self.onOk)
-      okButton.move(260, 110)
+      lay.addWidget(okButton, 2, 0)
       # now make the window visible
+      self.setLayout(lay)
       self.show()
       #
-
-   def onPushButton1(self):
-      if self.label4.isVisible():
-         self.label4.hide()
-      else:
-         self.label4.show()
 
    def onCancel(self):
        self.result = userCancelled
@@ -94,8 +159,29 @@ class GDMLColourMap(QtGui.QDialog) :
        self.result = userOK
        self.close()
 
-   def mouseMoveEvent(self,event):
-       self.label6.setText("X: "+str(event.x()) + " Y: "+str(event.y()))
+   def getGDMLMaterials(self):
+       matList = []
+       doc = FreeCAD.activeDocument()
+       try :
+          materials = doc.Materials
+       except :
+          from .importGDML import processXML
+          from .init_gui   import joinDir
+
+          print('Load Geant4 Materials XML')
+          processXML(doc,joinDir("Resources/Geant4Materials.xml"))
+          materials = doc.Materials
+
+       if materials != None :
+          for m in materials.OutList :
+              matList.append(m.Label)
+          #print(matList)
+
+       else :
+          print('Materials Not Found')
+
+       return matList      
+
 
 # Class definitions
 
@@ -103,6 +189,6 @@ class GDMLColourMap(QtGui.QDialog) :
 
 # Constant definitions
 global userCancelled, userOK
-userCancelled		= "Cancelled"
-userOK			= "OK"
+userCancelled           = "Cancelled"
+userOK                  = "OK"
 
--- freecad/gdml/exportGDML.py
+++ freecad/gdml/exportGDML.py
@@ -1617,7 +1617,9 @@ def exportWorldVol(vol) :
 
         processVols(vol, xmlVol, xmlParent, vol.Name, False)
 
-def exportGDML(first,filename) :
+def exportGDML(first,filename,type = 1) :
+    # type = 1 - Straight GDML export Tessellate
+    # type = 2 - Ignore Tessellate
     if filename.lower().endswith('.gdml') :
        # GDML Export
        print("\nStart GDML Export 0.1")
@@ -1625,16 +1627,92 @@ def exportGDML(first,filename) :
        GDMLstructure()
        zOrder = 1
        processMaterials()
-       exportWorldVol(first)
+       exportWorldVol(first, type)
        # format & write GDML file 
        indent(gdml)
        print("Write to GDML file")
        #ET.ElementTree(gdml).write(filename, 'utf-8', True)
        ET.ElementTree(gdml).write(filename,xml_declaration=True)
-       #ET.ElementTree(gdml).write(filename, pretty_print=True, xml_declaration=True)
+       #ET.ElementTree(gdml).write(filename, pretty_print=True, \
+       #xml_declaration=True)
        print("GDML file written")
     else :
        print('File extension must be gdml')
+
+def scanForStl(first, gxml, flag ):
+    # if flag == True ignore Parts that convert
+    
+   print('Scan For STL')    
+   while switch(first.TypeId) :
+      if case("Part::FeaturePython") : 
+         break
+
+      if case("App::Origin") :
+         #print("App Origin")
+         break
+
+      if case("App::GeoFeature") :
+         #print("App GeoFeature")
+         break
+
+      if case("App::Line") :
+         #print("App Line")
+         break
+
+      if case("App::Plane") :
+         #print("App Plane")
+         break
+      
+      break
+
+   if flag == True :
+      #
+      #  Now deal with objects that map to GDML solids
+      #
+      while switch(first.TypeId) :
+         if case("Part::Box") :
+            print("    Box")
+            break
+
+         if case("Part::Cylinder") :
+            print("    Cylinder")
+            break
+
+         if case("Part::Cone") :
+            print("    Cone")
+            break
+
+         if case("Part::Sphere") :
+            print("    Sphere")
+            break
+
+         break
+
+   # Deal with Booleans which will have Tool
+   if hasattr(first,'Tool') :
+      print(first.TypeId)
+      scanForStl(first.Base, gxml, flag)
+      scanForStl(first.Tool, gxml, flag)
+
+   if hasattr(first,'OutList') :
+      for obj in first.OutList :
+          scanForStl(obj, gxml, flag)
+
+   print('Write out stl')
+   print(first.Name)
+   print(first.Label)
+   print(first.TypeId)
+
+def exportGXML(first, path, flag) :
+    print('Path : '+path)
+    basename = os.path.basename(path)
+    gxml = ET.Element('gxml')
+    scanForStl(first, gxml, flag)
+    # format & write gxml file 
+    indent(gxml)
+    print("Write to gxml file")
+    ET.ElementTree(gxml).write(os.path.join(path,basename+'.gxml'))
+    print("gxml file written")
 
 def exportMaterials(first,filename) :
     if filename.lower().endswith('.xml') :
@@ -1650,18 +1728,69 @@ def exportMaterials(first,filename) :
     else :
        print('File extension must be xml')
 
-def export(exportList,filename) :
-    "called when FreeCAD exports a file"
-    
-    first = exportList[0]
-    if first.TypeId == "App::Part" :
-       exportGDML(first,filename)
+def create_gcard(path, flag) :
+    basename = os.path.basename(path)
+    print('Create gcard : '+basename)
+    print('Path : '+path)
+    gcard = ET.Element('gcard')
+    ET.SubElement(gcard,'detector',{'name':'target_cad','factory':'CAD'})
+    if flag == True :
+       ET.SubElement(gcard,'detector',{'name':'target_gdml','factory':'GDML'})
+    indent(gcard)
+    path = os.path.join(path,basename+'.gcard')
+    ET.ElementTree(gcard).write(path)
 
-    elif first.Name == "Materials" :
-       exportMaterials(first,filename)
-    
+def checkDirectory(path) :    
+    if not os.path.exists(path):
+       print('Creating Directory : '+path)
+       os.mkdir(path)
+
+def exportGEMC(first, path, flag) :
+    # flag = True  GEMC - GDML
+    # flag = false just CAD
+    print('Export GEMC')
+    print(flag)
+    print(path)
+    checkDirectory(path)
+    # Create CAD directory
+    cadPath = os.path.join(path,'cad')
+    checkDirectory(cadPath)
+    # Create gcard
+    create_gcard(path, flag)
+    exportGXML(first,cadPath,flag)
+    if flag == True :
+       print('Create GDML directory')
+       gdmlPath = os.path.join(path,'gdml')
+       checkDirectory(gdmlPath)
+       exportGDML(first, gdmlPath, 2)
+
+def export(exportList,filepath) :
+    "called when FreeCAD exports a file"
+  
+    print('Export')
+    first = exportList[0]
+ 
+    import os
+    path, fileExt = os.path.splitext(filepath)
+    print('filepath : '+path)
+    print('file extension : '+fileExt)
+
+    if fileExt == '.gemc' :
+       exportGEMC(first, path, False)
+
+    elif fileExt == '.GEMC' :
+       exportGEMC(first, path, True)
+
     else :
-       print("Need to a Part for export")
-       from PySide import QtGui
-       QtGui.QMessageBox.critical(None,'Need to select a Part for export','Press OK')
+       if first.TypeId == "App::Part" :
+          exportGDML(first,filepath)
+
+       elif first.Name == "Materials" :
+          exportMaterials(first,filepath)
+    
+       else :
+          print("Needs to be a Part for export")
+          from PySide import QtGui
+          QtGui.QMessageBox.critical(None,'Need to select a Part for export', \
+                 'Press OK')
 
