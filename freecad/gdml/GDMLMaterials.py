21c04383381ebcb19eeb1dbc64142ec06c9c2797
Keith Sloan <keith@sloan-home.co.uk>
2022-02-12 10:47:10 +0000

    Fix no materials

--- freecad/gdml/GDMLMaterials.py
+++ freecad/gdml/GDMLMaterials.py
@@ -50,7 +50,6 @@ def getMaterialsList():
     doc = FreeCAD.activeDocument()
     try:
         materials = doc.Materials
-        geant4 = doc.Geant4
         g4Mats = doc.getObject('G4Materials')
 
     except:
@@ -60,7 +59,6 @@ def getMaterialsList():
         print('Load Geant4 Materials XML')
         processGEANT4(doc, joinDir("Resources/Geant4Materials.xml"))
         materials = doc.Materials
-        geant4 = doc.Geant4
         g4Mats = doc.getObject('G4Materials')
 
     try:
@@ -100,9 +98,12 @@ def refreshG4Materials(doc):
 
 
 def newGetGroupedMaterials():
+    from .importGDML import joinDir, processGEANT4
     print('New getGroupedMaterials')
     from .GDMLObjects import GroupedMaterials
     doc = FreeCAD.activeDocument()
+    if not hasattr(doc, 'Materials') or not hasattr(doc, 'G4Materials'):
+        processGEANT4(doc, joinDir("Resources/Geant4Materials.xml"))
     docG4Materials = doc.G4Materials
     if not hasattr(docG4Materials, 'version'):
         refreshG4Materials(doc)
@@ -157,7 +158,8 @@ def getGroupedMaterials():
     doc = FreeCAD.activeDocument()
     docMaterials = doc.Materials
     matList = []
-    if docMaterials is not None:
+
+    if doc.Materials is not None:
         for m in docMaterials.OutList:
             if m.Label != "Geant4":
                 if m.Label not in matList:
