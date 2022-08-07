d47ef281cd102b80c6824da242282c94876cc67b
Keith Sloan <keith@sloan-home.co.uk>
2022-08-07 18:31:17 +0100

    remove stray x

--- freecad/gdml/importGDML.py
+++ freecad/gdml/importGDML.py
@@ -2253,7 +2253,7 @@ def setSkinSurface(doc, vol, surface):
     volObj = doc.getObject(vol)
     volObj.SkinSurface = surface
 
-x
+
 def processSurfaces(doc, volDict, structure):
     from .GDMLObjects import GDMLbordersurface
 
